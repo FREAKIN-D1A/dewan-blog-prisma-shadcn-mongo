3156fed73a16b48909893b4bcfae7cfb9707aa29
Freakin_D1A <d1adewanasad@gmail.com>
2023-12-15 07:52:40 +0600

    commit : 01.03.02 for production

--- app/page.js
+++ app/page.js
@@ -3,7 +3,46 @@ import { baseApiUrl } from "@/lib/config";
 
 import Image from "next/image";
 import { getSession } from "next-auth/react";
-import { getBlogs } from "@/lib/utils";
+// import { getBlogs } from "@/lib/utils";
+async function getBlogs(searchParams) {
+	// console.log("\n\n<<<<<=====<<<<<====<<<<<\n");
+	// console.log("searchParams  ===>\n");
+	// console.log(searchParams);
+	// console.log("\n>>>>>====>>>>>=====>>>>>>\n\n");
+
+	const urlParams = {
+		page: searchParams.page || 1,
+	};
+	const searchQuery = new URLSearchParams(urlParams).toString();
+	const fetchUrl = baseApiUrl + "/all/blogs/?" + searchQuery;
+
+	// console.log("\n\n<<<<<=====<<<<<====<<<<<\n");
+	// console.log("fetchUrl  ===>\n");
+	// console.log(fetchUrl);
+	// console.log("\n>>>>>====>>>>>=====>>>>>>\n\n");
+
+	try {
+		const response = await fetch(fetchUrl, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			// cache: "force-cache",
+		});
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch  blogs");
+		}
+
+		const data = await response.json();
+		return data; // {blogs, currentPage, totalPages}
+	} catch (error) {
+		console.log("\n\n<<<<<=====<<<<<====<<<<<\n");
+		console.log("error  ===>\n");
+		console.log(error);
+		console.log("\n>>>>>====>>>>>=====>>>>>>\n\n");
+	}
+}
 
 export default async function Home({ searchParams }) {
 	const { blogs, currentPage, totalPages } = await getBlogs(searchParams);
